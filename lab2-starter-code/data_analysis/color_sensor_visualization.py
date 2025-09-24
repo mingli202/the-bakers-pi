6f9ff5c4df715268d9ab3c66274191f3ac3f7dbb
mingli202 <vincentmingli@gmail.com>
2025-09-24 15:09:33 -0400

    purple

--- lab2-starter-code/data_analysis/color_sensor_visualization.py
+++ lab2-starter-code/data_analysis/color_sensor_visualization.py
@@ -16,7 +16,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 
 
-COLOR_SENSOR_DATA_FILE = "color_sensor_blue.csv"
+COLOR_SENSOR_DATA_FILE = "color_sensor_purple.csv"
 
 
 def gaussian(x, values):
